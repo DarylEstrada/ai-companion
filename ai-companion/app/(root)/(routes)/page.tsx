b4e2fe1f225ccfc253d5e51e9e593014c6486d9a
Daryl <destrada@jairosolutions.com>
2023-08-23 17:05:31 +0800

    Category Filter

---
+++ ai-companion/app/(root)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { SearchInput } from "@/components/search-input";
+
+const RootPage = () => {
+  return (
+    <div className="h-full pt-8 p-4 space-y-4">
+      <SearchInput />
+    </div>
+  );
+};
+
+export default RootPage;
